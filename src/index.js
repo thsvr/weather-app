f22f0d043357eea789b93a7a2727bbca083b9dfe
thsvr <vr.ths.zd@gmail.com>
2020-03-10 15:08:49 -0300

    Add some const to take classes form html and manipulate with js.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-
 const weatherKey = "6080e17b0fc07b99a71fde330eb48aed";
+const searchForm = document.querySelector(".check-by-city .search-form");
+const searchCity = document.querySelector(".check-by-city .search-city");
+const warning = document.querySelector(".check-by-city .warning");
+const location = document.querySelector(".card-city .location");
 
