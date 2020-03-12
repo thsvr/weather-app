4ce660fb6ad98d9c7ad45dc6a7f2faf968145818
thsvr <vr.ths.zd@gmail.com>
2020-03-12 20:03:43 -0300

    Refactor dom manipulation.

--- src/index.js
+++ src/index.js
@@ -16,46 +16,17 @@ let b = document.getElementById('temp-toggle');
 let c = document.getElementById("main-temp");
 let d = document.getElementById("btn-main-temp");
 
-
 // DOM MANIPULATION
-let index = 0;
+let index = -1;
+const dataArray = [];
 
 searchForm.addEventListener("submit", event => {
   event.preventDefault();
+  index++;
 
   let chosenCity = searchCity.value;
   const cityListed = location.querySelectorAll(".card-city .city");
-  const arrCities = Array.from(cityListed);
-
-  if (arrCities.length > 0) {
-    const newArrCity = arrCities.filter(element => {
-      let info = "";
-      if (chosenCity.includes(",")) {
-        
-        if (chosenCity.split(",")[1].length > 2) {
-          chosenCity = chosenCity.split(",")[0];
-          info = element
-            .querySelector(".city-country span") //allow more than 1 city
-            .textContent//.toLowerCase();
-
-        } else {
-          info = element.querySelector(".city-country").dataset.name//.toLowerCase();
-        }
-      } else {      
-        info = element.querySelector(".city-country span").textContent//.toLowerCase();        
-      }
-      return info == chosenCity//.toLowerCase();
-    });
-
-    if (newArrCity.length > 0) {
-      warning.textContent = `${
-        newArrCity[0].querySelector(".city-country span").textContent
-      } is displayed below but you can check for another location that isn't displayed yet.`;
-      searchForm.reset();
-      searchCity.focus();
-      return;
-    };    
-  }
+  const arrCities = Array.from(cityListed);   
 
   //PROMISE
 
@@ -77,37 +48,37 @@ searchForm.addEventListener("submit", event => {
   searchForm.reset();
   searchCity.focus();
 }); //END OF PROMISSE
+const domContainer = (data) => {
+  const { main, name, sys, weather } = data;
+  const info = getWholeData(data)
 
-      const card = `
-        <article class="city-country" data-name="${name},${sys.country}">
-          <span class="city-name">${name}</span>
-          <span class="country-name">${sys.country}</span>
-        </article> 
+      const list = document.createElement("li");
+      list.classList.add("city");
+  cityCountry[0].innerHTML = info.getPlace()[0]
+  cityCountry[1].innerHTML = info.getPlace()[1]
+  cityTemp.innerHTML = `<i class="fas fa-thermometer-three-quarters"></i>${Math.round(info.getTemp().temp)}°C`;
+  iconImg.src = `http://openweathermap.org/img/wn/${info.getWeather().icon}@2x.png`;
+  desc.innerHTML = info.getWeather().description;
 
-        <article class="city-temp">
-          <div id="switchFar"><i class="fas fa-thermometer-three-quarters"></i>${Math.round(main.temp)}°C</div>
-          <button onclick="switchTempToFar()" id="btn-test" class="btn-temp">Switch to °F</button>
-        </article>
+  extraInfo[0].innerHTML = `<i class="fas fa-thermometer-three-quarters c-yellow"></i>
+            <span class="x-padd">Feels Like</span>
+            ${Math.round(info.getTemp().feels_like)}°C`;
 
-        <article class="icon-container">
-          <img class="weather-icon" src="${icon}" alt="${
-        weather[0]["description"]}">
-          <p class="weather-desc">${weather[0]["description"]}</p>
-        </article>       
+  extraInfo[1].innerHTML = `<i class="fas fa-temperature-high c-red"></i>
+            <span class="x-padd">Max temp</span>${Math.round(info.getTemp().temp_max)}°C`;
 
-        <article class="extra-info">
-          <p><i class="fas fa-thermometer-three-quarters c-yellow"></i><span class="x-padd">Feels Like</span>${Math.round(main.feels_like)}°C</p>
-          <p><i class="fas fa-temperature-high c-red"></i><span class="x-padd">Max temp</span>${Math.round(main.temp_max)}°C</p>
-          <p><i class="fas fa-temperature-low c-blue"></i><span class="x-padd">Min temp</span>${Math.round(main.temp_min)}°C</p>
-        </article>  
+  extraInfo[1].innerHTML = `<i class="fas fa-temperature-low c-blue"></i>
+            <span class="x-padd">Min temp</span>${Math.round(info.getTemp().temp_min)}°C`;
 
-        <article class="kelvin-info">
-          <button onclick="switchTempToKel()" data-index="${++index}" id="btn-test${++index}" class="btn-temp">Switch to °K</button>
-          <div id="switchKel">${Math.round(main.temp)}°C</div>
-        </article> 
-        
-        <p class="info-by"><i class="fas fa-info-circle"></i><span class="x-padd">Info got by Open Weather App<span></p>
-        `;
+  a.innerHTML = `${Math.round(info.getTemp().temp)}°C`;
+  console.log(data)
+  d.style.display = 'block';
+  b.style.display = 'inline';
+  b.addEventListener('click', () => {toggleTemp(b, a, info.getTemp().temp)})
+  d.addEventListener('click', () => {toggleTempF(d, c, info.getTemp().temp)})
+}
+
+
 
         let checkTemp = false;
             
