518a157903d91dd2821a47ad1ffb3a23455215b2
thsvr <vr.ths.zd@gmail.com>
2020-03-13 17:04:51 -0300

    Revert linter that broked the code.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const weatherKey = '6080e17b0fc07b99a71fde330eb48aed';
+
 const searchForm = document.querySelector('.check-by-city .search-form');
 const searchCity = document.querySelector('.check-by-city .search-city');
 const warning = document.querySelector('.check-by-city .warning');
@@ -8,6 +9,7 @@ const desc = document.querySelector('.icon-container > p');
 const cityTemp = document.querySelector('.city-temp > div');
 const cityCountry = document.querySelectorAll('.city-country > span');
 const extraInfo = document.querySelectorAll('.extra-info > p');
+
 const a = document.getElementById('switchKel');
 const b = document.getElementById('temp-toggle');
 const c = document.getElementById('main-temp');
@@ -36,9 +38,11 @@ searchForm.addEventListener('submit', event => {
   // index += 1;
   const chosenCity = searchCity.value;
   // const cityListed = location.querySelectorAll('.card-city .city');
-  // const arrCities = Array.from(cityListed);
+  // const arrCities = Array.from(cityListed);   
+
   const openWeatherApp = `https://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&appid=${weatherKey}&units=metric`;
   // let wholeData;
+
   const domContainer = (data) => {
     // const { main, name, sys, weather } = data;
     const info = getWholeData(data);
@@ -91,15 +95,18 @@ searchForm.addEventListener('submit', event => {
     b.addEventListener('click', () => { toggleTemp(b, a, info.getTemp().temp) });
     d.addEventListener('click', () => { toggleTempF(d, c, info.getTemp().temp });
   };
+
   fetch(openWeatherApp)
     .then(response => response.json())
     .then(data => {
       dataArray.push(data);
+
       domContainer(data);
     })
     .catch(() => {
       warning.textContent = 'I\'m afraid we don\'t know the weather of this city.';
     });
+
   warning.textContent = '';
   searchForm.reset();
   searchCity.focus();
