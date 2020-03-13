923bed1868276b70649e06803d935ee5f052b131
thsvr <vr.ths.zd@gmail.com>
2020-03-12 23:17:28 -0300

    Fix simple mistakes.

--- src/index.js
+++ src/index.js
@@ -16,68 +16,6 @@ let b = document.getElementById('temp-toggle');
 let c = document.getElementById("main-temp");
 let d = document.getElementById("btn-main-temp");
 
-// DOM MANIPULATION
-let index = -1;
-const dataArray = [];
-
-searchForm.addEventListener("submit", event => {
-  event.preventDefault();
-  index++;
-
-  let chosenCity = searchCity.value;
-  const cityListed = location.querySelectorAll(".card-city .city");
-  const arrCities = Array.from(cityListed);   
-
-  //PROMISE
-
-  const openWeatherApp = `https://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&appid=${weatherKey}&units=metric`;
-  let wholeData;
-
-  fetch(openWeatherApp)
-    .then(response => response.json())
-    .then(data => {
-      dataArray.push(data);
-
-      domContainer(data);
-    })
-    //when some error occurs
-    .catch(() => {
-      warning.textContent = "I'm afraid we don't know the weather of this city.";
-    });
-  warning.textContent = "";
-  searchForm.reset();
-  searchCity.focus();
-}); //END OF PROMISSE
-const domContainer = (data) => {
-  const { main, name, sys, weather } = data;
-  const info = getWholeData(data)
-
-      const list = document.createElement("li");
-      list.classList.add("city");
-  cityCountry[0].innerHTML = info.getPlace()[0]
-  cityCountry[1].innerHTML = info.getPlace()[1]
-  cityTemp.innerHTML = `<i class="fas fa-thermometer-three-quarters"></i>${Math.round(info.getTemp().temp)}°C`;
-  iconImg.src = `http://openweathermap.org/img/wn/${info.getWeather().icon}@2x.png`;
-  desc.innerHTML = info.getWeather().description;
-
-  extraInfo[0].innerHTML = `<i class="fas fa-thermometer-three-quarters c-yellow"></i>
-            <span class="x-padd">Feels Like</span>
-            ${Math.round(info.getTemp().feels_like)}°C`;
-
-  extraInfo[1].innerHTML = `<i class="fas fa-temperature-high c-red"></i>
-            <span class="x-padd">Max temp</span>${Math.round(info.getTemp().temp_max)}°C`;
-
-  extraInfo[1].innerHTML = `<i class="fas fa-temperature-low c-blue"></i>
-            <span class="x-padd">Min temp</span>${Math.round(info.getTemp().temp_min)}°C`;
-
-  a.innerHTML = `${Math.round(info.getTemp().temp)}°C`;
-  console.log(data)
-  d.style.display = 'block';
-  b.style.display = 'inline';
-  b.addEventListener('click', () => {toggleTemp(b, a, info.getTemp().temp)})
-  d.addEventListener('click', () => {toggleTempF(d, c, info.getTemp().temp)})
-}
-
 const getWholeData = (data) => {
 
   const getWeather = () => {
@@ -92,8 +30,83 @@ const getWholeData = (data) => {
     return [name, country] = [data.name, data.sys.country];
   }
 
-
   return { getWeather, getTemp, getPlace }
+}
+
+let index = -1;
+const dataArray = [];
+
+searchForm.addEventListener("submit", event => {
+  event.preventDefault();
+  index++;
+
+  let chosenCity = searchCity.value;
+  const cityListed = location.querySelectorAll(".card-city .city");
+  const arrCities = Array.from(cityListed);   
+
+  const openWeatherApp = `https://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&appid=${weatherKey}&units=metric`;
+  // let wholeData;
+
+  fetch(openWeatherApp)
+    .then(response => response.json())
+    .then(data => {
+      dataArray.push(data);
+
+      domContainer(data);
+    })
+    
+    .catch(() => {
+      warning.textContent = "I'm afraid we don't know the weather of this city.";
+    });
+
+  warning.textContent = "";
+  searchForm.reset();
+  searchCity.focus();
+});
+
+const domContainer = (data) => {
+  //const { main, name, sys, weather } = data;
+  const info = getWholeData(data)
+  const list = document.createElement("li");
+  list.classList.add("city");
+  cityCountry[0].innerHTML = info.getPlace()[0]
+  cityCountry[1].innerHTML = info.getPlace()[1]
+  cityTemp.innerHTML = `<i class="fas fa-thermometer-three-quarters"></i>${Math.round(info.getTemp().temp)}°C`;
+  iconImg.src = `http://openweathermap.org/img/wn/${info.getWeather().icon}@2x.png`;
+  desc.innerHTML = info.getWeather().description;
+
+  extraInfo[0].innerHTML = `<i class="fas fa-thermometer-three-quarters c-yellow"></i>
+            <span class="x-padd">Feels Like</span>
+            ${Math.round(info.getTemp().feels_like)}°C`;
+
+  extraInfo[1].innerHTML = `<i class="fas fa-temperature-high c-red"></i>
+            <span class="x-padd">Max temp</span>${Math.round(info.getTemp().temp_max)}°C`;
+
+  extraInfo[2].innerHTML = `<i class="fas fa-temperature-low c-blue"></i>
+            <span class="x-padd">Min temp</span>${Math.round(info.getTemp().temp_min)}°C`;
+
+  a.innerHTML = `${Math.round(info.getTemp().temp)}°C`;
+  // console.log(data)
+  d.style.display = 'block';
+  b.style.display = 'inline';
+  b.addEventListener('click', () => {toggleTemp(b, a, info.getTemp().temp)})
+  d.addEventListener('click', () => {toggleTempF(d, c, info.getTemp().temp)})
+}
+
+let checkTempF = false;
+
+const toggleTempF = (elem, c, tempInfo) => {
+  let cel = `<i class="fas fa-thermometer-three-quarters"></i>${Math.round(tempInfo)}°C`;
+  let faren = `<i class="fas fa-thermometer-three-quarters"></i>${Math.round(tempInfo * (9 / 5) + 32)}°F`;
+    if (checkTempF) { 
+      c.innerHTML = cel;
+      elem.innerHTML = 'Switch to °F'; 
+      checkTempF = false;
+    } else {
+      c.innerHTML = faren;
+      elem.innerHTML = 'Switch to °C';
+      checkTempF = true;
+    }
 }
 
 let checkTemp = false;
@@ -112,18 +125,3 @@ const toggleTemp = (elem, c, tempInfo) => {
     }
 }
 
-let checkTempF = false;
-
-const toggleTempF = (elem, c, tempInfo) => {
-  let cel = `<i class="fas fa-thermometer-three-quarters"></i>${Math.round(tempInfo)}°C`;
-  let faren = `<i class="fas fa-thermometer-three-quarters"></i>${Math.round(tempInfo * (9 / 5) + 32)}°F`;
-    if (checkTempF) { 
-      c.innerHTML = cel;
-      elem.innerHTML = 'Switch to °F'; 
-      checkTempF = false;
-    } else {
-      c.innerHTML = faren;
-      elem.innerHTML = 'Switch to °C';
-      checkTempF = true;
-    }
-}
