9c23bffb6bfbcd91e62d5d11f61e67aca10a7e8e
Thaís Vieira <vr.ths.zd@gmail.com>
2020-03-19 13:14:21 -0300

    Revert "Far"

--- src/index.js
+++ src/index.js
@@ -1,22 +1,104 @@
-import DOM, { searchForm, searchCity, warning } from './DOM';
-import parseJSON from './parseJSON';
-
-
+/* eslint-disable semi */
+/* eslint-disable no-param-reassign */
+/* eslint-disable arrow-body-style */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable object-curly-newline */
+/* eslint-disable object-curly-spacing */
+/* eslint-disable camelcase */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-undef */
+/* eslint-disable no-return-assign */
+/* eslint-disable arrow-parens */
 const weatherKey = '6080e17b0fc07b99a71fde330eb48aed';
+const searchForm = document.querySelector('.check-by-city .search-form');
+const searchCity = document.querySelector('.check-by-city .search-city');
+const warning = document.querySelector('.check-by-city .warning');
+const iconImg = document.querySelector('.icon-container > img');
+const desc = document.querySelector('.icon-container > p');
+const cityTemp = document.querySelector('.city-temp > div');
+const cityCountry = document.querySelectorAll('.city-country > span');
+const extraInfo = document.querySelectorAll('.extra-info > p');
+const a = document.getElementById('switchKel');
+const b = document.getElementById('temp-toggle');
+const c = document.getElementById('main-temp');
+const d = document.getElementById('btn-main-temp');
 
+/* eslint no-return-assign: ["error", "always"] */
+const getWholeData = (data) => {
+  // eslint-disable-next-line arrow-body-style
+  const getWeather = () => {
+    return {description, icon, main} = data.weather[0];
+  };
+  const getTemp = () => {
+    return {temp, temp_max, temp_min, feels_like} = data.main;
+  };
+  // eslint-disable-next-line arrow-body-style
+  const getPlace = () => {
+    // eslint-disable-next-line no-global-assign
+    return [name, country] = [data.name, data.sys.country];
+  };
+  return { getWeather, getTemp, getPlace };
+};
 const dataArray = [];
 searchForm.addEventListener('submit', event => {
   event.preventDefault();
   const chosenCity = searchCity.value;
   const openWeatherApp = `https://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&appid=${weatherKey}&units=metric`;
-  const openWeatherAppF = `https://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&appid=${weatherKey}&units=imperial`;
-
+  const domContainer = (data) => {
+    const info = getWholeData(data);
+    const list = document.createElement('li');
+    let checkTempF = false;
+    const toggleTempF = (elem, cc, tempInfo) => {
+      const cel = `<i class='fas fa-thermometer-three-quarters'></i>${Math.round(tempInfo)}°C`;
+      const faren = `<i class='fas fa-thermometer-three-quarters'></i>${Math.round(tempInfo * (9 / 5) + 32)}°F`;
+      if (checkTempF) {
+        cc.innerHTML = cel;
+        elem.innerHTML = 'Switch to °F';
+        checkTempF = false;
+      } else {
+        cc.innerHTML = faren;
+        elem.innerHTML = 'Switch to °C';
+        checkTempF = true;
+      }
+    };
+    let checkTemp = false;
+    const toggleTemp = (elem, ccc, tempInfo) => {
+      const cel = `<i class='fas fa-thermometer-three-quarters'></i>${Math.round(tempInfo)}°C`;
+      const kel = `<p>${Math.round((tempInfo) + 273.15)}°K</p>`;
+      if (checkTemp) {
+        ccc.innerHTML = cel;
+        elem.innerHTML = 'Switch to °K';
+        checkTemp = false;
+      } else {
+        ccc.innerHTML = kel;
+        elem.innerHTML = 'Switch to °C';
+        checkTemp = true;
+      }
+    };
+    list.classList.add('city');
+    cityCountry[0].innerHTML = info.getPlace()[0];
+    cityCountry[1].innerHTML = info.getPlace()[1];
+    cityTemp.innerHTML = `<i class='fas fa-thermometer-three-quarters'></i>${Math.round(info.getTemp().temp)}°C`;
+    iconImg.src = `http://openweathermap.org/img/wn/${info.getWeather().icon}@2x.png`;
+    desc.innerHTML = info.getWeather().description;
+    extraInfo[0].innerHTML = `<i class='fas fa-thermometer-three-quarters c-yellow'></i>
+              <span class='x-padd'>Feels Like</span>
+              ${Math.round(info.getTemp().feels_like)}°C`;
+    extraInfo[1].innerHTML = `<i class='fas fa-temperature-high c-red'></i>
+              <span class='x-padd'>Max temp</span>${Math.round(info.getTemp().temp_max)}°C`;
+    extraInfo[2].innerHTML = `<i class='fas fa-temperature-low c-blue'></i>
+              <span class='x-padd'>Min temp</span>${Math.round(info.getTemp().temp_min)}°C`;
+    a.innerHTML = `${Math.round(info.getTemp().temp)}°C`;
+    d.style.display = 'block';
+    b.style.display = 'inline';
+    b.addEventListener('click', () => { toggleTemp(b, a, info.getTemp().temp) });
+    d.addEventListener('click', () => { toggleTempF(d, c, info.getTemp().temp) });
+  };
   fetch(openWeatherApp)
     .then(response => response.json())
-    .then((json) => {
-      dataArray.push(json);
-      const data = parseJSON(json);
-      DOM(data);
+    .then(data => {
+      dataArray.push(data);
+      domContainer(data);
     })
     .catch(() => {
       warning.textContent = 'I\'m afraid we don\'t know the weather of this city.';
